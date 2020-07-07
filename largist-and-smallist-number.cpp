b069112ad6a9a28430fe692d0a80b691b06b30ce
kulwinder <33957493+kulwinder96@users.noreply.github.com>
2020-07-07 15:41:57 +0530

    fixed some bugs

--- largist-and-smallist-number.cpp
+++ largist-and-smallist-number.cpp
@@ -5,18 +5,16 @@ int main()
 {
 vector<int>v{-1, -234, -867,2,4,44, 76,12,6,7,9,123, 456, 765,0};
 int large=0,small=0;
+int index=v[0];
 for(unsigned int i=0; i<v.size(); i++)
 {
-for(unsigned int j=0; j<v.size(); j++)
+if(v[i]>large)
 {
-if(v[i] > v[j]&& v[i] >large)
-{
-large = v[i];
+large=v[i];
 }
-if(v[i] < v[j] &&v[i] < small)
+if(v[i] <small)
 {
 small=v[i];
-}
 }
 }
 cout<<"largist number is "<<large;
--- largist-number.cpp
+++ largist-number.cpp
@@ -4,15 +4,12 @@ using namespace std;
 int main()
 {
 vector<int>v{2,4,44, 76,12,6,7,9,123, 456, 765,0};
-int result=0;
+int result=v[0];
 for(unsigned int i=0; i<v.size(); i++)
 {
-for(unsigned int j=0; j<v.size(); j++)
+if(v[i] >result)
 {
-if(v[i] > v[j]&& v[i] >result)
-{
-result = v[i];
-}
+result=v[i];
 }
 }
 cout<<"largist number is "<<result;
